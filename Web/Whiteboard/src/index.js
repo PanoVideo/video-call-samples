e53aae486fca07848bd6de4652fe60312570844b
villee.wang <wangfeiys@163.com>
2020-08-17 14:01:49 +0800

    update whiteboard demo

--- Web/Whiteboard/src/index.js
+++ Web/Whiteboard/src/index.js
@@ -15,18 +15,23 @@ const userNameInput = document.getElementById('userName')
 userIdInput.value = Math.floor(Math.random() * 10000000) + ''
 userNameInput.value = `pano-${userIdInput.value}`
 
+let rtcWhiteboard
 function onOpenWhiteboard(whiteboardClient) {
+  if (!whiteboardClient) {
+    console.log('open whiteboard failed, no whiteboard instance.')
+    return
+  }
   whiteboardClient.open(whiteboardWrapper)
   panoControlPanel.style.display = 'none'
   whiteboardWrapper.style.display = 'block'
   closeBtn.style.display = 'block'
   whiteboardClient && Toolbar.install(whiteboardClient)
 }
-let whiteboardInstance
+const rtcEngine = new Panortc.RtcEngine(document.getElementById('appId'))
 
 joinBtn.onclick = async () => {
-  if (whiteboardInstance) {
-    onOpenWhiteboard(whiteboardInstance)
+  if (rtcWhiteboard) {
+    onOpenWhiteboard(rtcWhiteboard)
     return
   }
   joinBtn.disabled = true
@@ -37,94 +42,95 @@ joinBtn.onclick = async () => {
   })
   const { appId, channelId, userId, name, panoToken } = joinParams
 
-  const whiteboard = new Panortc.RtcWhiteboard()
-  whiteboard.joinAndConnect({
-    appId,
-    panoToken,
-    channelId,
-    name,
-    userId,
-    onSuccess(rtcWhiteboard) {
-      whiteboardInstance = rtcWhiteboard
-      window.rtcWhiteboard = rtcWhiteboard
-      console.log('whiteboardClient connect success')
-      // 隐藏 join channel 面板
-      joinForm.style.display = 'none'
-      joinBtn.disabled = false
-      joinBtn.innerHTML = preBtnText
-
-      // 显示 pano 控制面板
-      // 在这个面板里可以手动打开 whiteboard
-      panoControlPanel.style.display = 'block'
-      // 添加处理打开面板的方法，这个方法会在收到打开面板指令时自动调用
-      // 正常情况下，如果用户加入的会议里已经有其他用户打开白板，那么别的用户会在刚加入 channel 时立即收到打开白板的指令
-      // 如果没有人打开过白板，那么用户可以手动打开白板，sdk 会自动发送打开指令给别的 client
-      openWhiteboardBtn.addEventListener('click', () =>
-        onOpenWhiteboard(rtcWhiteboard)
-      )
-
-      closeBtn.addEventListener('click', () => {
-        rtcWhiteboard.close()
-        whiteboardWrapper.style.display = 'none'
-        panoControlPanel.style.display = 'none'
-        panoControlPanel.style.display = 'block'
-        closeBtn.style.display = 'none'
-      })
-
-      rtcWhiteboard.on(Panortc.RtcWhiteboard.Events.messageReceived, (data) => {
-        console.log('receive custom message:', data)
-      })
-      rtcWhiteboard.on(
-        Panortc.RtcWhiteboard.Events.whiteboardFailover,
-        (payload) => {
-          console.error('got failover event', payload)
-        }
-      )
-      rtcWhiteboard.on(Panortc.RtcWhiteboard._WS_FAILOVER, reconnect)
-      rtcWhiteboard.on(
-        Panortc.RtcWhiteboard.Events.readyStateChanged,
-        (payload) => {
-          console.log(
-            'demo app readyStateChanged: ',
-            payload.ready,
-            ',message: ',
-            payload.message
-          )
-          if (!payload.ready) {
-            alert(payload.message)
-          }
-        }
-      )
-
-      function reconnect() {
-        console.error('handle whiteboard failover')
-        setTimeout(() => {
-          whiteboard.joinAndConnect({
-            userId,
-            appId,
-            panoToken,
-            channelId,
-            name,
-            onSuccess: () => {
-              console.log('failover reconnect success')
-            },
-            onFail: () => {
-              console.log('failover reconnect failed')
-              reconnect()
-            }
-          })
-        }, 3000)
-      }
+  rtcEngine.joinChannel(
+    {
+      appId,
+      token: panoToken,
+      channelId,
+      channelMode: PanoRtc.Constants.ChannelMode.TYPE_MEETING,
+      userId,
+      userName: name
     },
-    onFail(result) {
-      joinBtn.disabled = false
-      joinBtn.innerHTML = preBtnText
-      console.log('channel join failed', result)
+    {
+      joinChannelType: PanoRtc.Constants.JoinChannelType.whiteboardOnly
     }
-  })
-  whiteboard.on(Panortc.RtcWhiteboard.Events.openStateChanged, () =>
-    onOpenWhiteboard(whiteboard)
   )
 }
+
+rtcEngine.on(PanoRtc.RtcEngine.Events.joinChannelConfirm, (data) => {
+  if (data.result !== 'success') {
+    window.alert(`join channel failed because: ${data.message}`)
+    return
+  }
+  console.log('join channel success!')
+})
+
+rtcEngine.on(PanoRtc.RtcEngine.Events.whiteboardAvailable, (data) => {
+  console.log('demo app: whiteboardAvailable', data)
+  rtcWhiteboard = rtcEngine.getWhiteboard()
+  window.rtcWhiteboard = rtcWhiteboard
+  console.log('whiteboardClient connect success')
+  // 隐藏 join channel 面板
+  joinForm.style.display = 'none'
+  joinBtn.disabled = false
+  joinBtn.innerHTML = preBtnText
+
+  // 显示 pano 控制面板
+  // 在这个面板里可以手动打开 whiteboard
+  panoControlPanel.style.display = 'block'
+  // 添加处理打开面板的方法，这个方法会在收到打开面板指令时自动调用
+  // 正常情况下，如果用户加入的会议里已经有其他用户打开白板，那么别的用户会在刚加入 channel 时立即收到打开白板的指令
+  // 如果没有人打开过白板，那么用户可以手动打开白板，sdk 会自动发送打开指令给别的 client
+  openWhiteboardBtn.addEventListener('click', () =>
+    onOpenWhiteboard(rtcWhiteboard)
+  )
+
+  closeBtn.addEventListener('click', () => {
+    rtcWhiteboard.close()
+    whiteboardWrapper.style.display = 'none'
+    panoControlPanel.style.display = 'none'
+    panoControlPanel.style.display = 'block'
+    closeBtn.style.display = 'none'
+  })
+
+  rtcWhiteboard.on(Panortc.RtcWhiteboard.Events.openStateChanged, () =>
+    onOpenWhiteboard(rtcWhiteboard)
+  )
+
+  rtcWhiteboard.on(Panortc.RtcWhiteboard.Events.messageReceived, (data) => {
+    console.log('receive custom message:', data)
+  })
+  rtcWhiteboard.on(
+    Panortc.RtcWhiteboard.Events.whiteboardFailover,
+    (payload) => {
+      console.error('got failover event', payload)
+      if (payload.state === 'Success') {
+        onOpenWhiteboard(rtcWhiteboard)
+      }
+    }
+  )
+
+  rtcWhiteboard.on(
+    Panortc.RtcWhiteboard.Events.readyStateChanged,
+    (payload) => {
+      console.log(
+        'demo app readyStateChanged: ',
+        payload.ready,
+        ',message: ',
+        payload.message
+      )
+      if (!payload.ready) {
+        alert(payload.message)
+      }
+    }
+  )
+})
+
+rtcEngine.on(PanoRtc.RtcEngine.Events.whiteboardUnavailable, (data) => {
+  console.log('demo app: whiteboardUnavailable', data)
+  whiteboardWrapper.style.display = 'none'
+  panoControlPanel.style.display = 'none'
+  closeBtn.style.display = 'none'
+})
 
 window.Panobridge = Panortc.PanoBridge
