1b9c93bfba8d1e500dc49bef9470c4433723ced8
markma <markma@pano.video>
2020-12-10 16:40:01 +0800

    add quadrilateral video transform gesture operation

--- iOS/MagicVideo/MagicVideo/ViewController.m
+++ iOS/MagicVideo/MagicVideo/ViewController.m
@@ -24,6 +24,7 @@ static NSString * _server = @"api.pano.video";
 
 @property (strong, nonatomic) PanoRtcEngineKit * engineKit;
 @property (assign, nonatomic) BOOL magicEnabled;
+@property (assign, nonatomic) PanoQuadIndex quadTransformIndex;
 
 @end
 
@@ -62,21 +63,10 @@ static NSString * _server = @"api.pano.video";
 #pragma mark Gesture Methods
 
 - (void)initGestureRecognizers {
-    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handleScalingGesture:)];
-    [self.view addGestureRecognizer:pinchGesture];
-
-    UIRotationGestureRecognizer *rotateGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotateZGesture:)];
-    [self.view addGestureRecognizer:rotateGesture];
-
-    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotateXYGesture:)];
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleQuadMoveGesture:)];
     panGesture.maximumNumberOfTouches = 1;
     panGesture.minimumNumberOfTouches = 1;
     [self.view addGestureRecognizer:panGesture];
-
-    UIPanGestureRecognizer *panGestureTwo = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleProjectionDepthGesture:)];
-    panGestureTwo.maximumNumberOfTouches = 2;
-    panGestureTwo.minimumNumberOfTouches = 2;
-    [self.view addGestureRecognizer:panGestureTwo];
 
     UITapGestureRecognizer *doubleTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleResetTransParamsGesture:)];
     doubleTapGesture.numberOfTapsRequired = 2;
@@ -84,85 +74,38 @@ static NSString * _server = @"api.pano.video";
     [self.view addGestureRecognizer:doubleTapGesture];
 }
 
-- (void)handleScalingGesture: (UIPinchGestureRecognizer *)pinchRecognizer {
-    CGFloat xScale = 1.0f;
-    CGFloat yScale = 1.0f;
-    double theSlope;
+- (void)handleQuadMoveGesture:(UIPanGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        CGPoint touchLoc = [sender locationInView:self.view];
+        CGFloat halfWidth = self.view.frame.size.width/2.0f;
+        CGFloat halfHeight = self.view.frame.size.height/2.0f;
+        if (touchLoc.x<halfWidth && touchLoc.y<halfHeight) {
+            self.quadTransformIndex = TopLeft;
+        } else if (touchLoc.x>=halfWidth && touchLoc.y<halfHeight) {
+            self.quadTransformIndex = TopRight;
+        } else if (touchLoc.x<halfWidth && touchLoc.y>=halfHeight) {
+            self.quadTransformIndex = BottomLeft;
+        } else {
+            self.quadTransformIndex = BottomRight;
+        }
+    } else if (sender.state == UIGestureRecognizerStateChanged) {
+        CGPoint dist = [sender translationInView:self.view];
+        PanoQuadTransformOption * option = [[PanoQuadTransformOption alloc] init];
+        option.enable = self.magicEnabled;
+        option.index = self.quadTransformIndex;
+        option.xDeltaAxis = dist.x/self.view.frame.size.width;
+        option.yDeltaAxis = dist.y/self.view.frame.size.height;
+        option.bMirror = [self.engineKit isFrontCamera] ? YES : NO;
+        [self.engineKit setOption:option forType:kPanoOptionQuadTransform];
+    }
 
-    if ([pinchRecognizer state] == UIGestureRecognizerStateBegan ||
-            [pinchRecognizer state] == UIGestureRecognizerStateChanged) {
-        if ([pinchRecognizer numberOfTouches] > 1) {
-            UIView *theView = [pinchRecognizer view];
-            CGPoint locationOne = [pinchRecognizer locationOfTouch:0 inView:theView];
-            CGPoint locationTwo = [pinchRecognizer locationOfTouch:1 inView:theView];
-            if (locationOne.x == locationTwo.x) {
-                    // perfect vertical line
-                    // not likely, but to avoid dividing by 0 in the slope equation
-                theSlope = 1000.0;
-            }else if (locationOne.y == locationTwo.y) {
-                    // perfect horz line
-                    // not likely, but to avoid any problems in the slope equation
-                theSlope = 0.0;
-            }else {
-                theSlope = (locationTwo.y - locationOne.y)/(locationTwo.x - locationOne.x);
-            }
-            double abSlope = (theSlope>0 ? theSlope : -theSlope);
-            if (abSlope < 0.5) {
-                //  Horizontal pinch - scale in the X
-                xScale = pinchRecognizer.scale;
-            }else if (abSlope > 1.7) {
-                // Vertical pinch - scale in the Y
-                yScale = pinchRecognizer.scale;
-            } else {
-                // Diagonal pinch - scale in both directions
-                xScale = yScale = pinchRecognizer.scale;
-            }  // else for diagonal pinch
-        }  // if numberOfTouches
-    }  // StateBegan if
-
-    PanoBuiltinTransformOption * option = self.baseTransformOption;
-    option.xScaling = xScale;
-    option.yScaling = yScale;
-    [self.engineKit setOption:option forType:kPanoOptionBuiltinTransform];
-    pinchRecognizer.scale = 1;
-}
-
-- (void)handleRotateZGesture: (UIRotationGestureRecognizer *) sender {
-    PanoBuiltinTransformOption * option = self.baseTransformOption;
-    option.zRotation = sender.rotation;
-    [self.engineKit setOption:option forType:kPanoOptionBuiltinTransform];
-}
-
-- (void)handleRotateXYGesture: (UIPanGestureRecognizer *) sender {
-    CGPoint dist = [sender translationInView:self.view];
-    PanoBuiltinTransformOption * option = self.baseTransformOption;
-    option.xRotation = dist.x;
-    option.yRotation = dist.y;
-    [self.engineKit setOption:option forType:kPanoOptionBuiltinTransform];
     [sender setTranslation:CGPointZero inView:self.view];
 }
 
-- (void)handleProjectionDepthGesture: (UIPanGestureRecognizer *) sender {
-    CGPoint dist = [sender translationInView:self.view];
-    PanoBuiltinTransformOption * option = self.baseTransformOption;
-    if (dist.x>0) {
-        option.xProjection = 0.05;
-    } else if (dist.x<0) {
-        option.xProjection = -0.05;
-    }
-    if (dist.y>0) {
-        option.yProjection = 0.05f;
-    } else if (dist.y<0) {
-        option.yProjection = -0.05f;
-    }
-    [self.engineKit setOption:option forType:kPanoOptionBuiltinTransform];
-    [sender setTranslation:CGPointZero inView:self.view];
-}
-
--(void)handleResetTransParamsGesture: (UITapGestureRecognizer *) sender {
-    PanoBuiltinTransformOption * option = self.baseTransformOption;
+-(void)handleResetTransParamsGesture:(UITapGestureRecognizer *)sender {
+    PanoQuadTransformOption *option = [[PanoQuadTransformOption alloc] init];
     option.bReset = true;
-    [self.engineKit setOption:option forType:kPanoOptionBuiltinTransform];
+    [self.engineKit setOption:option forType:kPanoOptionQuadTransform];
 }
 
 #pragma mark Private Methods
